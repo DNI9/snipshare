afb7cc56ce003330720ac66f4b7075a74eb71765
DNI9 <itsr9boi@gmail.com>
2022-03-21 00:41:08 +0530

    pick profile data from provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,15 +1,24 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { NextApiHandler } from 'next';
-import NextAuth from 'next-auth';
+import NextAuth, { NextAuthOptions } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 
 import { prisma } from '~/lib/prisma';
 
-const options = {
+const options: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      profile(profile) {
+        return {
+          id: profile.id,
+          name: profile.name,
+          email: profile.email,
+          username: profile.login,
+          image: profile.avatar_url,
+        };
+      },
     }),
   ],
   adapter: PrismaAdapter(prisma),
