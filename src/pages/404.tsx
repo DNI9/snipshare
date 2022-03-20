5abcb42557c5e7cad7b3f3158bcd0dcfb6eac432
DNI9 <itsr9boi@gmail.com>
2022-03-20 22:03:50 +0530

    add meta to 404 paage

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,37 +2,46 @@ import { Heading, Text, Button, Flex } from '@chakra-ui/react';
 import { AiFillHome } from 'react-icons/ai';
 
 import { NextLink } from '~/components/core';
+import { Meta } from '~/layout';
 
 export default function NotFound() {
   return (
-    <Flex align="center" justify="center" minH={'100vh'} flexDirection="column">
-      <Heading
-        display="inline-block"
-        as="h1"
-        size="4xl"
-        bgGradient="linear(to-r, blue.400, purple.300)"
-        backgroundClip="text"
+    <>
+      <Meta title="Oh no! A 404 Page" />
+      <Flex
+        align="center"
+        justify="center"
+        minH={'100vh'}
+        flexDirection="column"
       >
-        404
-      </Heading>
-      <Text fontSize="18px" mt={3} mb={2}>
-        Page Not Found
-      </Text>
-      <Text color={'gray.500'} mb={6}>
-        The page you are looking for does not seem to exist
-      </Text>
-
-      <NextLink href="/">
-        <Button
-          leftIcon={<AiFillHome size={20} />}
-          _hover={{ bg: 'blue.300' }}
-          bg="blue.400"
-          color="white"
-          variant="solid"
+        <Heading
+          display="inline-block"
+          as="h1"
+          size="4xl"
+          bgGradient="linear(to-r, blue.400, purple.300)"
+          backgroundClip="text"
         >
-          Go to Home
-        </Button>
-      </NextLink>
-    </Flex>
+          404
+        </Heading>
+        <Text fontSize="18px" mt={3} mb={2}>
+          Page Not Found
+        </Text>
+        <Text color={'gray.500'} mb={6}>
+          The page you are looking for does not seem to exist
+        </Text>
+
+        <NextLink href="/">
+          <Button
+            leftIcon={<AiFillHome size={20} />}
+            _hover={{ bg: 'blue.300' }}
+            bg="blue.400"
+            color="white"
+            variant="solid"
+          >
+            Go to Home
+          </Button>
+        </NextLink>
+      </Flex>
+    </>
   );
 }
