20606e96b3049155eed13310a0f37d11d0f91fb1
DNI9 <itsr9boi@gmail.com>
2022-03-23 19:50:24 +0530

    chore: update db seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,23 +8,33 @@ import { codes } from './data';
 const getRandomFromArray = <T = any>(arr: Array<T>) =>
   arr[Math.floor(Math.random() * arr.length)];
 
-const snippetData: Prisma.SnippetCreateInput[] = [];
+const USERS = [
+  { name: 'dni9', id: 'cl10yr8kl00107wacydab8cn5' },
+  { name: 'ind', id: 'cl13elwsb0357aoaca9nortaj' },
+];
 
 async function main() {
-  codes.forEach(({ title, content, language, description }) => {
-    snippetData.push({
-      title,
-      content,
-      description,
-      language,
-      isPrivate: getRandomFromArray<boolean>([true, false]),
-      user: { connect: { id: 'cl10yr8kl00107wacydab8cn5' } },
-    });
-  });
+  const { count } = await prisma.snippet.deleteMany({});
+  console.log(`${count} snippets deleted`);
 
-  for (const snippet of snippetData) {
-    const res = await prisma.snippet.create({ data: snippet });
-    console.log(`Snippet created: ${res.id}`);
+  for (const user of USERS) {
+    const snippetData: Prisma.SnippetCreateInput[] = [];
+
+    codes.forEach(({ title, content, language, description }) => {
+      snippetData.push({
+        title: `[${user.name}] ${title}`,
+        content,
+        description,
+        language,
+        isPrivate: getRandomFromArray<boolean>([true, false]),
+        user: { connect: { id: user.id } },
+      });
+    });
+
+    for (const snippet of snippetData) {
+      const res = await prisma.snippet.create({ data: snippet });
+      console.log(`Snippet created: ${res.id}`);
+    }
   }
 }
 
