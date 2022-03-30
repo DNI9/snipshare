abd6377515f5a3c53cdf3821029e108bca0c72cf
DNI9 <itsr9boi@gmail.com>
2022-03-30 15:06:57 +0530

    fix: add limit to snippet

--- src/schema/snippet.ts
+++ src/schema/snippet.ts
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 export const SnippetSchema = yup
   .object({
     title: yup.string().required().min(2).max(100),
-    content: yup.string().required('Snippet code is required'),
+    content: yup.string().max(5000).required('Snippet code is required'),
     language: yup.string().required(),
     description: yup.string().max(500),
     isPrivate: yup.boolean(),
