8c70aea2c695137ebed111353742a13af70ae90e
DNI9 <itsr9boi@gmail.com>
2022-04-08 00:06:07 +0530

    fix: show empty message for search query

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid } from '@chakra-ui/react';
+import { Center, SimpleGrid, Text } from '@chakra-ui/react';
 import type { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/react';
 
@@ -6,28 +6,48 @@ import { Pagination } from '~/components/core';
 import { SnippetCard } from '~/components/snippet';
 import { AppLayout, Meta } from '~/layout';
 import { getPublicSnippets } from '~/services/snippet';
-import { SnippetData } from '~/types/snippet';
+import { SnippetData, SnippetType } from '~/types/snippet';
 import { getQueryString } from '~/utils/next';
 
 type Props = {
   data: SnippetData;
+  search: string | undefined;
 };
 
-export default function Explore({ data }: Props) {
+const EmptyMessage = ({ search }: { search: string }) => (
+  <Center flexDir="column">
+    <Text fontSize="2xl" mt={3} mb={2}>
+      No results
+    </Text>
+    <Text color={'gray.500'} mb={6}>
+      There is nothing with <strong>{search}</strong>
+    </Text>
+  </Center>
+);
+
+const Snippets = ({ data }: { data: SnippetType[] }) => (
+  <SimpleGrid my={3} columns={1} spacing={5}>
+    {data.map(snippet => (
+      <SnippetCard
+        isPublic
+        key={snippet.id}
+        snippet={snippet}
+        isSnippetOwner={snippet.isSnippetOwner}
+      />
+    ))}
+  </SimpleGrid>
+);
+
+export default function Explore({ data, search }: Props) {
   return (
     <>
       <Meta title="Explore SnipShare" />
       <AppLayout containerProps={{ maxW: 'container.md' }}>
-        <SimpleGrid my={3} columns={1} spacing={5}>
-          {data.snippets.map(snippet => (
-            <SnippetCard
-              isPublic
-              key={snippet.id}
-              snippet={snippet}
-              isSnippetOwner={snippet.isSnippetOwner}
-            />
-          ))}
-        </SimpleGrid>
+        {search && !data.snippets.length ? (
+          <EmptyMessage search={search} />
+        ) : (
+          <Snippets data={data.snippets} />
+        )}
         <Pagination
           totalPages={data.totalPages}
           currentPage={data.currentPage}
@@ -56,5 +76,7 @@ export const getServerSideProps: GetServerSideProps = async ({
     page,
   });
 
-  return { props: { data } };
+  if (!!data.totalResults && page > data.totalPages) return { notFound: true };
+
+  return { props: { data, search: search || '' } };
 };
