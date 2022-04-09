89a7069362dbbc4c1e4c37571c00c66f67f7c002
DNI9 <itsr9boi@gmail.com>
2022-04-09 23:52:25 +0530

    feat: add nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 
 import { appTheme } from '~/lib/theme';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <SessionProvider session={pageProps.session}>
     <ChakraProvider theme={appTheme}>
+      <NextNProgress options={{ showSpinner: false }} />
       <Component {...pageProps} />
     </ChakraProvider>
   </SessionProvider>
